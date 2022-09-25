684202447272e7471209a8bc1f06be1808f6a4e2
yy <bloxed7@gmail.com>
2022-09-25 04:44:39 +0200

    final

--- src/pages/client/Homepage/RandomAlbumsPreview.js
+++ src/pages/client/Homepage/RandomAlbumsPreview.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { AlbumList } from "../../../components";
 
 export default function RandomAlbumsPreview() {
-    const [albums, setAlbums] = useState([]);
-    setAlbums([]);
+    // const [albums, setAlbums] = useState([]);
+    // setAlbums([]);
     return (
         <div className="grid gap-4 grid-cols-2 sm:grid-cols-4 ">
-            <AlbumList albums={albums} />
+            <AlbumList albums={[]} />
         </div>
     );
 }
